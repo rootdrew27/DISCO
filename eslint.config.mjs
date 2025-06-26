957b7e02081f8098a49026eb633dcd8fef5f5f3b
Andrew <root.drew27@gmail.com>
2025-06-26 13:33:06 -0500

    Additional specifications for eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,8 +11,16 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
-  eslintConfigPrettier
+    ...compat.extends("next/core-web-vitals", "next/typescript"),
+    eslintConfigPrettier,
+    ...compat.config({
+      extends: ['next'],
+      settings: {
+        next: {
+          rootDir: 'src/',
+        }
+      }
+    })
 ];
 
 export default eslintConfig;
